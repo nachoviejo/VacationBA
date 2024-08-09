c92ffb21a52330a2dcaffa3b026d764b44604eb8
iviejo_genasys <iviejo@genasys.com>
2024-08-09 12:45:48 -0300

    Initial Commit

---
+++ VacationInBuenosAires/VacationInBuenosAiresApp.swift
@@ -0,0 +1,17 @@
+//
+//  VacationInBuenosAiresApp.swift
+//  VacationInBuenosAires
+//
+//  Created by Kinamic Kinamic on 09/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct VacationInBuenosAiresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
